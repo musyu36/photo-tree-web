488c9501d4cbd1aec09e285222523b1d632b0905
mimura <musyu36@gmail.com>
2020-04-01 19:21:31 +0900

    linter変更

--- pages/index.vue
+++ pages/index.vue
@@ -2,40 +2,31 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">
-        photo-tree-web
-      </h1>
-      <h2 class="subtitle">
-        My cat&#39;s pajamas Nuxt.js project
-      </h2>
+      <h1 class="title">photo-tree-web</h1>
+      <h2 class="subtitle">My cat&#39;s pajamas Nuxt.js project</h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
+          >Documentation</a
         >
-          Documentation
-        </a>
         <a
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
           class="button--grey"
+          >GitHub</a
         >
-          GitHub
-        </a>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Logo from "~/components/Logo.vue";
 
 export default {
   components: {
-    Logo
-  }
-}
+    Logo,
+  },
+};
 </script>
 
 <style>
@@ -54,8 +45,8 @@ export default {
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
