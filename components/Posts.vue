8d0a50c5ee8b43f1961c52e100a5c4aa2d5b8b77
mimura <musyu36@gmail.com>
2020-04-08 20:53:18 +0900

    投稿をdbに保存

--- components/Posts.vue
+++ components/Posts.vue
@@ -9,11 +9,30 @@
         <div class="back-btn vertical-middle">
           <img src="/images/back.svg" class="h-4" />
         </div>
+        <div class="post-btn" @click="post">
+          Post
+        </div>
       </div>
       <div class="modal_content p-8">
-        <el-upload action="" :show-file-list="false" :http-request="uploadFile">
+        <div class="flex justify-center">
+          <img :src="imageUrl" class="uploaded-image" alt="" />
+        </div>
+        <el-upload
+          v-if="!imageUrl"
+          action=""
+          :show-file-list="false"
+          :http-request="uploadFile"
+        >
           <el-button size="small" type="primary">Click to upload</el-button>
         </el-upload>
+        <el-input
+          type="textarea"
+          :rows="8"
+          placeholder="Please input"
+          class="mt-8"
+          v-model="text"
+        >
+        </el-input>
       </div>
     </div>
   </div>
@@ -30,6 +49,8 @@ export default {
   data() {
     return {
       posts: [],
+      imageUrl: null,
+      text: null,
     };
   },
   methods: {
@@ -39,6 +60,21 @@ export default {
       //どこに保存するか, 名前が一意になるように時間情報を加える
       const ref = storageRef.child(`posts/${time}_${data.file.name}`);
       const snapshot = await ref.put(data.file);
+
+      //アップロード後にURLを取得
+      const url = await snapshot.ref.getDownloadURL();
+      this.imageUrl = url;
+    },
+    async post() {
+      await db.collection("posts").add({
+        text: this.text,
+        image: this.imageUrl,
+        createdAt: new Date().getTime(),
+      });
+      this.modalVisible = false;
+      this.text = null;
+      this.imageUrl = null;
+      window.alert("保存されました");
     },
   },
   mounted() {
@@ -55,3 +91,11 @@ export default {
   },
 };
 </script>
+
+<style>
+.post-btn {
+  background-color: white;
+  color: black;
+  cursor: pointer;
+}
+</style>
